7d4aea299096c9ebe2a356870c2d06804fde6e1a
jonathan perez <perezgradosj@gmail.com>
2021-03-27 11:35:56 -0500

    add unique to phone in user table

---
+++ Omega.Ticket.Infraestructure/Data/Migrations/20210327003338_JP_AddUniqueToPhone_UserTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Omega.Ticket.Infraestructure.Data.Migrations
+{
+    public partial class JP_AddUniqueToPhone_UserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "UQ_Phone",
+                schema: "Seg",
+                table: "User",
+                column: "Phone",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UQ_Phone",
+                schema: "Seg",
+                table: "User");
+        }
+    }
+}
